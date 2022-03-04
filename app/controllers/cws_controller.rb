984c231f9ff4e64324f14a084bf34fff152e74bb
Manh <vdmanh107@gmail.com>
2022-03-04 14:27:52 +0700

    Update cws_controller.rb

--- app/controllers/cws_controller.rb
+++ app/controllers/cws_controller.rb
@@ -13,22 +13,28 @@ class CwsController < ApplicationController
     # get room id and body of the message
     room_id = params[:webhook_event][:room_id]
     body = params[:webhook_event][:body]
-     
-    return if room_id == 116821963
     
-    # find room
-    c = ChatWork::Room.find room_id: room_id
-    room_name = c.name
+    # case room is Nghiệp Inc chat room, we send message to slack
+    if room_id == 116821963
+      body = "New message in Nghiệp Inc chat: \n
+      #{body}"
+      
+      render status: 200
+    else 
+      # find room
+      c = ChatWork::Room.find room_id: room_id
+      room_name = c.name
 
-    # set up some params
-    destination_room_id = ENV["DESTINATION_ROOM_ID"]
-    body = "[To:3056978] Vu Duc Manh (private)\nMessage in #{room_name}:\n#{body}"
+      # set up some params
+      destination_room_id = ENV["DESTINATION_ROOM_ID"]
+      body = "[To:3056978] Vu Duc Manh (private)\nMessage in #{room_name}:\n#{body}"
 
-    # change token to bot's token
-    ChatWork.api_key = ENV["BOT_TOKEN"]
+      # change token to bot's token
+      ChatWork.api_key = ENV["BOT_TOKEN"]
 
-    # create message from bot
-    ChatWork::Message.create room_id: destination_room_id, body: body
-    render status: 200
+      # create message from bot
+      ChatWork::Message.create room_id: destination_room_id, body: body
+      render status: 200
+    end
   end
 end
